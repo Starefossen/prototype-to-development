d014f31dcd44dbc680a5b0610bbb0b279e7aa7c0
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-03-13 10:23:37 +0100

    Watch all them files 🔎

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,21 @@ module.exports = function(grunt) {
           'css/style.css': 'css/style.scss'
         }
       }
-    }
+    },
+
+    watch: {
+      sass: {
+        files: ['css/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['build']);
   grunt.registerTask('build', ['sass']);
